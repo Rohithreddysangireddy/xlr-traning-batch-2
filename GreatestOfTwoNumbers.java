ef970baeb1e7f2a759fe9842f069d0408af649d5
Rohithreddysangireddy <rohithreddysangireddy@gmail.com>
2025-01-02 12:14:43 +0530

    Update GreatestOfTwoNumbers.java

--- GreatestOfTwoNumbers.java
+++ GreatestOfTwoNumbers.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class GreatestOfTwoNumbers {
+public class demo2 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         
