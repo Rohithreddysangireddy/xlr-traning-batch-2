dfb9db34554bb11fa3ebf70cdac524c47583f4f1
Rohithreddysangireddy <rohithreddysangireddy@gmail.com>
2025-01-03 10:11:00 +0530

    Add files via upload
    java code your name and password verfication

---
+++ Demo6.java
@@ -0,0 +1,11 @@
+public class Demo6 {
+    public static void main(String[] args) {
+        String username = "Rohithreddy";
+        String password = "Rohith@2801";
+        while((username == "Rohithreddy") && (password == "Rohith@2801")) {
+            System.out.print("welcome rohith reddy");
+            break;
+        
+        }
+    }
+}
